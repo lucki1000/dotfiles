3fcec8bf44647be3be3102fc8e7bda71c7c3fe78
Lukas Bonrath <ludwig.germersheim@gmail.com>
2021-07-05 22:05:24 +0200

    indentations

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ do
 done
 
 install_deps () {
-    echo ":: Install dependencies        ::"
+    echo ":: Install dependencies                  ::"
     if [[ sys_installer == "pacman" ]]
     then 
         sudo pacman -Sy curl git zsh neofetch fzf --noconfirm &>/dev/null
@@ -30,28 +30,29 @@ install_deps () {
     then
         sudo zypper --non-interactive in curl git zsh neofetch fzf &>/dev/null
     fi
-    echo ":: Finished installation of dependencies ::"
+    echo ":: Dependencies are installed            ::"
 }
 
 #Install dependencies
 install_deps
 
 #OhMyZsh
-echo ":: Install OhMyZsh                 ::"
+echo ":: Install OhMyZsh                           ::"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &>/dev/null
 #Install Powerline 10K
-echo ":: Install Powerline 10K           ::"
+echo ":: Install Powerline 10K                     ::"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k &>/dev/null
 #Install Zsh Syntax Highlighting
-echo ":: Install Zsh Syntax Highlighting ::"
+echo ":: Install Zsh Syntax Highlighting           ::"
 cd ~ 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git &>/dev/null
 #Install Zsh Autosuggestions
-echo ":: Install Zsh Autosuggest         ::"
+echo ":: Install Zsh Autosuggest                   ::"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &>/dev/null
 #Load my .zshrc
-echo ":: load .zshrc                     ::"
+echo ":: load .zshrc                               ::"
 wget -O ~/.zshrc https://raw.githubusercontent.com/lucki1000/dotfiles/main/_zshrc &>/dev/null
 #Set P10K theme
-echo ":: load p10k settings              ::"
+echo ":: load p10k settings                        ::"
 wget -O ~/.p10k.zsh https://raw.githubusercontent.com/lucki1000/dotfiles/main/_p10k.zsh &>/dev/null
+echo ":: Finished                                  ::"
