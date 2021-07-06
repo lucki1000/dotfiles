c112149c3950eaab3fe1aa225450c29addf72378
Lukas Bonrath <ludwig.germersheim@gmail.com>
2021-07-06 22:07:23 +0200

    indentations & set zsh as default

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ osInfo[/etc/debian_version]=apt
 for f in ${!osInfo[@]}
 do
     if [[ -f $f ]];then
-        echo ":: Package manager: ${osInfo[$f]}            ::"
+        #echo ":: Package manager: ${osInfo[$f]}                ::"
         sys_installer=${osInfo[$f]}
     fi
 done
@@ -20,25 +20,27 @@ install_deps () {
     echo ":: Install dependencies                      ::"
     if [[ $sys_installer == "pacman" ]]
     then 
-        sudo pacman -Sy curl git zsh neofetch fzf --noconfirm &>/dev/null
+        sudo pacman -Sy curl git zsh neofetch fzf --noconfirm
     elif [[ $sys_installer == "apt" ]]
     then 
-        sudo apt update && sudo apt install curl git zsh neofetch fzf -y &>/dev/null
+        sudo apt update && sudo apt install curl git zsh neofetch fzf -y
     elif [[ $sys_installer == "yum" ]]
     then
-        sudo yum -y install curl zsh git neofetch fzf &>/dev/null
+        sudo yum -y install curl zsh git neofetch fzf
     elif [[ $sys_installer == "zypper" ]]
     then
-        sudo zypper --non-interactive in curl git zsh neofetch fzf &>/dev/null
+        sudo zypper --non-interactive in curl git zsh neofetch fzf
+    else
+        exit
     fi
     echo ":: Dependencies are installed                ::"
-}
+} > /dev/null 2>&1
 
 #Install dependencies
 install_deps
 
 #Path to ZSH
-zshell_path=${which zsh}
+#zshell_path=$(which zsh)
 #OhMyZsh
 echo ":: Install OhMyZsh                           ::"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &>/dev/null
@@ -59,5 +61,6 @@ wget -O ~/.zshrc https://raw.githubusercontent.com/lucki1000/dotfiles/main/_zshr
 echo ":: load p10k settings                        ::"
 wget -O ~/.p10k.zsh https://raw.githubusercontent.com/lucki1000/dotfiles/main/_p10k.zsh &>/dev/null
 echo ":: Set ZSH as default SHELL                  ::"
+echo ":: Enter your password:                      ::"
 chsh -s $(which zsh)
 echo ":: Finished                                  ::"
